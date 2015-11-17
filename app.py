49a160eae5fcfc02911ea081b3272b941ff63e51
Pegasus Wang <tianma201211@gmail.com>
2015-11-17 18:29:39 +0800

    add new tofetch and motor client

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from config import redis_config
-from redis import StrictRedis
+from lib import _db
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
@@ -16,7 +15,8 @@ from urls import url_patterns
 class ArticlesApp(tornado.web.Application):
     def __init__(self):
         tornado.web.Application.__init__(self, url_patterns, **settings)
-        self._redis = StrictRedis(redis_config.HOST, redis_config.PORT)
+        self._redis = _db.redis_client
+        self._motor = _db.motor_client
 
 
 def main():
--- lib/_db.py
+++ lib/_db.py
@@ -4,11 +4,15 @@
 import _env
 from config.config import CONFIG
 from pymongo import MongoClient
+from motor import MotorClient
+from redis import StrictRedis
 
 
-client = MongoClient(CONFIG.MONGO.HOST, CONFIG.MONGO.PORT)
+mongo_client = MongoClient(CONFIG.MONGO.HOST, CONFIG.MONGO.PORT)
+motor_client = MotorClient(CONFIG.MONGO.HOST, CONFIG.MONGO.PORT)
+redis_client = StrictRedis(CONFIG.REDIS.HOST, CONFIG.REDIS.PORT)
 
 
-def get_collection(db_name, collection_name):
+def get_collection(db_name, collection_name, client=mongo_client):
     db = getattr(client, db_name, None)
     return getattr(db, collection_name, None)
