c81c713be8f891c0282fc20e11425bab40fe1b18
Pegasus Wang <tianma201211@gmail.com>
2015-12-09 19:20:23 +0800

    fix migrate to ghost

--- crawler/jb51/jb51_to_ghost.py
+++ crawler/jb51/jb51_to_ghost.py
@@ -10,6 +10,29 @@ from lib._db import get_collection
 def cur_timestamp():
     return int(time.time() * 1000)
 
+users = [
+    {
+        "id":           1,
+        "name":         "man",
+        "slug":         "man",
+        "email":        "user@example.com",
+        "image":        None,
+        "cover":        None,
+        "bio":          None,
+        "website":      None,
+        "location":     None,
+        "accessibility": None,
+        "status":       "active",
+        "language":     "zh_CN",
+        "meta_title":   None,
+        "meta_description": None,
+        "last_login":   None,
+        "created_at":   1283780649000,
+        "created_by":   1,
+        "updated_at":   1286958624000,
+        "updated_by":   1
+    }
+]
 
 def replace_post(post_data):
     d = {
@@ -17,22 +40,28 @@ def replace_post(post_data):
         "title":        "my blog post title",
         "slug":         "my-blog-post-title",
         "markdown":     "the *markdown* formatted post body",
-        "html":         "the <i>html</i> formatted post body",
-        "image":        null,
+        #"html":         "the <i>html</i> formatted post body",
+        "image":        None,
         "featured":     0,
         "page":         0,
         "status":       "published",
-        "language":     "en_US",
-        "meta_title":   null,
-        "meta_description":null,
-        "author_id":    1, // the first user created has an id of 1
-        "created_at":   1283780649000, // epoch time in millis
-        "created_by":   1, // the first user created has an id of 1
-        "updated_at":   1286958624000, // epoch time in millis
-        "updated_by":   1, // the first user created has an id of 1
-        "published_at": 1283780649000, // epoch time in millis
-        "published_by": 1 // the first user created has an id of 1
+        "language":     "zh_CN",
+        "meta_title":   None,
+        "meta_description": None,
+        "author_id":    1,
+        "created_at":   cur_timestamp(),
+        "created_by":   1,
+        "updated_at":   cur_timestamp(),
+        "updated_by":   1,
+        "published_at": cur_timestamp(),
+        "published_by": 1
     }
+    d['id'] = int(post_data['source_url'].rsplit('/', 1)[1].split('.')[0])
+    d['title'] = post_data['title']
+    d['slug'] = post_data['title'].replace(' ', '-').lower()
+    d['markdown'] = post_data['content']
+    return d
+
 
 def migrate():
     res = {
@@ -41,15 +70,39 @@ def migrate():
             "version": "003"
         }
     }
-
-
-def get_posts():
     coll = get_collection('test', 'articles')
 
-    for doc in coll.find().batch_size(1000):
+    posts = []
+    tags = []
+    posts_tags = []
+    tag_id = 0
 
+    #for doc in coll.find().batch_size(1000):
+    doc = coll.find_one()
+    if doc:
+        posts.append(replace_post(doc))
+        post_id = int(doc['source_url'].rsplit('/', 1)[1].split('.')[0])
+        tag_list = doc.get('tag_list')
+        tag = tag_list[0] if tag_list else ''
+        if tag:
+            tag_id += 1
+            tags.append({
+                "id": tag_id,
+                "name": tag,
+                "slug": tag.replace(' ', '-'),
+                "description": ""
+            })
+            posts_tags.append({"tag_id": tag_id, "post_id": post_id})
 
+    data = {
+        "posts": posts,
+        "tags": tags,
+        "posts_tags": posts_tags,
+        "users": users
+    }
+    res["data"] = data
+    return res
 
 
 if __name__ == '__main__':
-    main()
+    print(migrate())
